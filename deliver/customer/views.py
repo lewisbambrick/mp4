d66af4159a8f53c3207fe72815ed6dd54d8f1fe8
lewisbambrick <lewis.bambrick@gmail.com>
2022-02-03 19:13:10 +0000

    Order payment added

--- deliver/customer/views.py
+++ deliver/customer/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+import json
+from django.shortcuts import render, redirect
 from django.views import View
+from django.core.mail import send_mail
 from .models import MenuItem, Category, OrderModel
 
 
@@ -74,9 +76,49 @@ class Order(View):
             )
         order.items.add(*item_ids)
 
+        #after everything is done send confirmation email to user.
+        body= (' Thank you for your order! Your food is being made and will be delivered soon!\n'
+                f'Your Total: {price}\n'
+                'Thank you again for your order' 
+        )
+        
+        send_mail(
+            'Thank You For Your Order!',
+            body,
+            'example@email.ie',
+            [email],
+            fail_silently=False
+        )
+
         context = {
             'items': order_items['items'],
             'price': price
         }
 
+        return redirect('order-confirmation', pk=order.pk)
+
+class OrderConfirmation(View):
+    def get(self, request, pk, *args, **kwargs):
+        order = OrderModel.objects.get(pk=pk)
+
+        context = {
+            'pk': order.pk,
+            'items': order.items,
+            'price': order.price
+        }
+
         return render(request, 'customer/order_confirmation.html', context)
+    
+    def post(self, request, pk, *args, **kwargs):
+        data = json.loads(request.body)
+
+        if data['isPaid']:
+            order = OrderModel.objects.get(pk=pk)
+            order.is_paid = True
+            order.save()
+
+        return redirect('payment-confirmation')
+
+class OrderPayConfirmation(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'customer/order_pay_confirmation.html')
